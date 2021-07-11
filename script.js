fde41981b771f75b6093e5b05b05211da782cb31
mhdsajjad <mhdsajjadhasan@outlook.com>
2021-07-11 17:33:10 +0600

    2nd commit

--- script.js
+++ script.js
@@ -9,14 +9,50 @@ $(document).ready(function(){
 
         $('#menu').removeClass('fa-times');
         $('.navbar').removeClass('nav-toggle');
-
+    
         if($(window).scrollTop() > 0){
-            $('header').addClass('sticky');
+          $('header').addClass('sticky');
         }else{
-            $('header').removeClass('sticky');
+          $('header').removeClass('sticky');
         }
+    
+        if($(window).scrollTop() > 0){
+          $('.scroll-top').show();
+        }else{
+          $('.scroll-top').hide();
+        }
+    
+        // scroll spy 
+    
+        $('section').each(function(){
+    
+          let top = $(window).scrollTop();
+          let offset = $(this).offset().top - 200;
+          let id = $(this).attr('id');
+          let height = $(this).height();
+    
+          if(top > offset && top < offset + height){
+            $('.navbar a').removeClass('active');
+            $('.navbar').find(`[href="#${id}"]`).addClass('active');
+          }
+    
+        });
+    
+      });
 
-    });
+//smooth scrolling
 
+$('a[href*="#"]').on('click',function(e){
+
+  $('html, body').animate({
+
+    scrollTop : $($(this).attr('href')).offset().top,
+
+  },
+    500,
+    'linear'
+  );
+
+});
 
 });
